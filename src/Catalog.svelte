43f0c19a9bdcba6dfaa089be8ad390443ab44f15
petelina17 <petelina17@gmail.com>
2020-02-20 00:41:47 +0100

    catalog grey boxes

---
+++ src/Catalog.svelte
@@ -0,0 +1,29 @@
+<script>
+    import FrameBox from "./FrameBox.svelte";
+    const list = ['mountain', 'ocean', 'sea', 'desert', 'forest', 'sky', 'sunrise', 'sunset',
+    'cat', 'dog', 'horse', 'pig', 'fox', 'bird', 'rabbit', 'city', 'road', 'car', 'park',
+    'burger', 'beer', 'coffe', 'cake', 'ice-cream']
+</script>
+
+<div class="catalog">
+    {#each list as frame, i}
+    <div class="frame-box">
+        <FrameBox title={frame} />
+    </div>
+    {/each}
+</div>
+
+<style>
+    .catalog {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        height: 100%;
+    }
+
+   .frame-box {
+       width: 300px;
+       height: 300px;
+       margin: 5px 5px;
+   }
+</style>
---
+++ src/FrameBox.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let title
+</script>
+
+<div class="frame">
+    {title}
+</div>
+
+<style>
+    .frame {
+        background-color: #dddddd;
+        font-size: 3rem;
+        height: 100%;
+        width: 100%;
+    }
+</style>
